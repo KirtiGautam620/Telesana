43f1c9b106d1b2d2b6b81dc318f833a53c72122d
Shriti Negi <shritinegi24@gmail.com>
2025-10-30 23:28:02 +0530

    added utils , fix controllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,36 +1,33 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const {PrismaClient} = require('../generated/prisma')
-const prisma = new PrismaClient()
+const prisma =require('../utils/prisma.js')
 
 async function signup(req, res) {
-    let {username, email, password} = req.body
-    email = email.toLowerCase()
+    const {username, email, password} = req.body
     try{
-        const existingUser = await prisma.patient.findUnique({where: {email}})
+        const existingUser = await prisma.user.findUnique({where: {email}})
 
         if (existingUser){
             return res.status(400).json({message: "Email already registered"})
         }
 
         const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = await prisma.patient.create({
+        const newUser = await prisma.user.create({
             data: {username, email, password: hashedPassword}
         })
         return res.status(201).json({message: "Registration successful", user: {id: newUser.id, username: newUser.username, email: newUser.email}})
 
-    } catch(error) {
+    } catch(error){
         console.error(error)
         return res.status(500).json({error: "Something went wrong"})
     }
 }
 
 async function login(req, res) {
-    let {email, password} = req.body
-    email = email.toLowerCase()
+    const {email, password} = req.body
     try{
-        const existingUser = await prisma.patient.findUnique({where: {email}})
+        const existingUser = await prisma.user.findUnique({where: {email}})
         if (!existingUser){
             return res.status(400).json({message: "User not found"})
         } 
@@ -43,7 +40,7 @@ async function login(req, res) {
         const token = jwt.sign({id: existingUser.id}, process.env.JWT_SECRET, {expiresIn: '7d'})
         return res.status(200).json({message: "Login successful", token, user: {id: existingUser.id, username: existingUser.username, email: existingUser.email}})
 
-    } catch(error) {
+    } catch(error){
         console.error(error)
         return res.status(500).json({error: "Something went wrong"})
     }
