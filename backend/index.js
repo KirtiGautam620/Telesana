4fd26b460058d787835814dce794cd49aa38b25b
KIRTI G <kirtig@kirtis-MacBook-Pro.local>
2025-12-19 15:31:11 +0530

    backend: add healh check route

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,10 @@ app.use(cors({
 }))
 app.use(express.json())
 
+app.get("/api/healthcheck", (req, res) => {
+    res.status(200).json({ status: "OK", message: "Health check passed" });
+});
+
 app.use('/api/auth', authRoutes)
 app.use('/api/doctor', doctorRoutes)
 app.use('/api/patient', patientRoutes)
