a4f53859c761d1ed94cebbe68003422ff93daf80
KIRTI G <kirtig@kirtis-MacBook-Pro.local>
2026-01-16 19:29:41 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin:"https://telesana.onrender.com",
+    origin:"*",
     methods:["GET","POST","PUT","DELETE"],
 }))
 app.use(express.json())
